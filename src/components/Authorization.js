52449f3b9231a9d1a395bfdc0daeb6975b5de414
Daria Nikulina <daryanickulina@yandex.ru>
2022-07-15 14:52:01 +0300

    fix: Authorization

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -4,26 +4,30 @@ const checkResponse = (response) => {
   return response.ok ? response.json() : Promise.reject(`Ошибка ${response.status}`);
 }
 
-export const register = (username, password, email, calGoal) => {
-  return fetch(`${BASE_URL}/auth/local/register`, {
+export const register = (data) => {
+  return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ username, password, email, ru_cal_goal: calGoal })
+    body: JSON.stringify({
+      password: data.password,
+      email: data.email
+    })
   })
     .then(checkResponse)
 };
 
-export const authorize = (identifier, password) => {
-  return fetch(`${BASE_URL}/auth/local`, {
+export const authorize = (data) => {
+  return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ identifier, password })
+    body: JSON.stringify({
+      password: data.password,
+      email: data.email
+    })
   })
     .then(checkResponse)
 };
@@ -32,10 +36,9 @@ export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
     }
   })
     .then(checkResponse)
-}
+};
