1e3905b0ec14eab16cd551d7060a7196255b5742
Daria Nikulina <daryanickulina@yandex.ru>
2022-08-15 21:41:45 +0300

    feat: adds user's routers

--- controllers/user.js
+++ controllers/user.js
@@ -2,24 +2,57 @@ const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  // res.send(req.body)
-  // console.log(req.body)
+
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
 };
 
 module.exports.getUser = (req, res) => {
-  res.send(req.body)
-  // res.findById(req.params.userId)
-  //   .then(user => res.send({ data: user }))
-  //   .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findById(req.params.userId)
+    .then(user => res.send({ data: user }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
 }
 
 module.exports.getUsers = (req, res) => {
-  // res.send(users)
+  User.find({})
+    .then(user => res.send({ data: user }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+}
 
-  // res.find({})
-  //   .then(user => res.send({ data: user }))
-  //   .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+module.exports.updateProfileInfo = (req, res) => {
+  const userID = req.user._id;
+
+  User.findByIdAndUpdate(
+    userID,
+    {
+      name: req.body.name,
+      about: req.body.about
+    },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true
+    }
+  )
+    .then(info => res.send({ data: info }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+}
+
+module.exports.updateProfilePhoto = (req, res) => {
+  const userID = req.user._id;
+
+  User.findByIdAndUpdate(
+    userID,
+    {
+      avatar: req.body.avatar,
+    },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true
+    }
+  )
+    .then(info => res.send({ data: info }))
+    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
 }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 const userRouters = require('express').Router();
-const { createUser, getUser, getUsers } = require('../controllers/user');
+const { createUser, getUser, getUsers, updateProfileInfo, updateProfilePhoto } = require('../controllers/user');
 
 userRouters.post('/users', createUser);
 
-userRouters.get('/users/:userId', getUser)
+userRouters.get('/users/:userId', getUser);
 
-userRouters.get('/users', getUsers)
+userRouters.get('/users', getUsers);
+
+userRouters.patch('/me/avatar', updateProfilePhoto);
+
+userRouters.patch('/me', updateProfileInfo);
 
 module.exports = { userRouters }
