c6aee6afebf64fce5bfded8f6f4100be5ad3eaf9
Daria Nikulina <daryanickulina@yandex.ru>
2022-08-16 18:30:10 +0300

    feat: adds error's catch

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,41 @@
 const User = require('../models/user');
 
+const UndefinedError = 404;
+const invalidDataError = 400;
+const otherError = 500;
+
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+    .catch((err) => {
+      if (err.name === "ValidationError") return res.status(invalidDataError).send({ message: "Переданы некорректные данные" })
+      res.status(otherError).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` })
+    });
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
+    .orFail(() => {
+      const error = new Error('Запрашиваемый пользователь не найден');
+      error.name("UndefinedError");
+      throw error
+    })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+    .catch((err) => {
+      if (err.name === "UndefinedError") return res.status(UndefinedError).send({ message })
+      if (err.name === "TypeError") return res.status(invalidDataError).send({ message: "Переданы некорректные данные" })
+      res.status(otherError).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` })
+    });
 }
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+    .catch((err) => {
+      res.status(otherError).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` })
+    });
 }
 
 module.exports.updateProfileInfo = (req, res) => {
@@ -36,7 +54,9 @@ module.exports.updateProfileInfo = (req, res) => {
     }
   )
     .then(info => res.send({ data: info }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+    .catch((err) => {
+      res.status(otherError).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` })
+    });
 }
 
 module.exports.updateProfilePhoto = (req, res) => {
@@ -54,5 +74,7 @@ module.exports.updateProfilePhoto = (req, res) => {
     }
   )
     .then(info => res.send({ data: info }))
-    .catch(err => res.status(500).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` }));
+    .catch((err) => {
+      res.status(otherError).send({ message: `Произошла ошибка: ${err.message}, ${err.name}` })
+    });
 }
